1b204f3ce17693507954f9a5c7d259f49d7ac6a0
Sourec <crmyers@wpi.edu>
2016-10-04 12:10:00 -0400

    Add killer.sh for testing programs with memory leaks...

---
+++ killer.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+./"$1" &
+while (( $(grep MemFree /proc/meminfo | awk '{print $2}') > 1500000 ))
+do
+	echo `grep MemFree /proc/meminfo | awk '{print $2}'` kb free!
+done
+echo RACKET IS USING TOO MUCH MEMORY, ABORTING!
+kill -9 $(ps aux | grep "[r]acket" | awk '{print $2}')
